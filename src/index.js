69c990e09503e6503570eaf19a457b3aa2330402
ps807 <66031843+ps807@users.noreply.github.com>
2020-09-25 14:05:35 +0200

    END

--- src/index.js
+++ src/index.js
@@ -6,3 +6,16 @@
 //E.g.
 //Created by Angela Yu.
 //Copyright 2019.
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "Peter";
+const year = new Date().getFullYear();
+
+ReactDOM.render(
+  <div>
+    <p>Created by {name}</p>
+    <p>Copyright {year}</p>
+  </div>,
+  document.getElementById("root")
+);
